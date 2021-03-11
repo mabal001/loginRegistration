37b910ee08016873afa7f80a3fb2637475983a56
mabal001 <55331447+mabal001@users.noreply.github.com>
2021-03-10 20:18:34 -0500

    Add files via upload

---
+++ validation.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+
+$con = mysqli_connect('localhost', 'root', '1962');
+
+
+mysqli_select_db($con, 'userregistration');
+
+$name = $_POST['user'];
+$pass = $_POST['password'];
+$email = $_POST['email'];
+
+$s = " select * from usertable where name = '$name' && password = '$pass'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    $_SESSION['username'] = $name;
+    header('location:home.php');
+    
+}else{
+    header('location:login.php');
+ 
+}
+
+?>
